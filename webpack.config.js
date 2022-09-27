7c509f4a528bcb4336c812ba42dc51f6df8f12d4
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2022-09-27 23:55:52 +0200

    completed project using webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Lists To Do',
+      template: 'src/template.html',
+    }),
+  ],
+};
